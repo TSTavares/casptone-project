54d34d9679fa59410227a299d4dff362db0e140b
Thayana Tavares <thayanatavares09@gmail.com>
2023-07-15 19:41:24 +1200

    Created a login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -31,42 +31,70 @@ export default function LoginPage() {
   };
 
   return (
-    
     <main>
-      <div>
-        <h1 className={styles.titleLoginPage}>Money Minder</h1>
-        <p>Income & Expense Tracker</p>
-
         <div className={styles.navigation}>
-          <Link href="/">
-            <Button>Home</Button>
-          </Link>
-          <Link href="/categories">
-            <Button>Categories</Button>
-          </Link>
-          <Link href="/profile">
-            <Button>Profile</Button>
-          </Link>
-          <Link href="/login">
-            <Button>Login</Button>
-          </Link>
-        </div>
+            <Link href="/">
+              <Button>Home</Button>
+            </Link>
+            <Link href="/categories">
+              <Button>Categories</Button>
+            </Link>
+            <Link href="/profile">
+              <Button>Profile</Button>
+            </Link>
+            <Link href="/login">
+              <Button>Login</Button>
+            </Link>
         </div>
 
+    
         <div className={styles.container}>
-          <Card title="User Login" className={styles.card}>
-            <div className={styles.usernameLogin}>
-              <label>Username:</label>
-              <input type="text" value={username} onChange={handleUsernameChange} />
+            <div className={styles.leftContent}>
+                <div className={styles.logoContainer}>
+                <Image
+                className={styles.LoginImage}
+                loader={({ src }) => src}
+                src="/image2/Image14 - money.png"
+                alt="Photo Money Minder"
+                width={450}
+                height={450}
+                />
+                </div>
             </div>
-            <div className={styles.passwordLogin}>
-              <label>Password:</label>
-              <input type="password" value={password} onChange={handlePasswordChange} />
-            </div>
-            <button className={styles.loginButton}onClick={handleLogin}>Login</button>
-          </Card>
+
+            <div className={styles.rightContent}>
+                <div className={styles.loginContainer}>
+                    <h1 className={styles.titleLoginPage}>Money Minder</h1>
+                    <p>Income &amp; Expense Tracker</p>
+                    
+
+                    <Card title="User Login" className={styles.card}>
+                        <Image
+                        className={styles.logoImageLoginPage}
+                        loader={({ src }) => src}
+                        src="/image/logomoneyminder.jpg"
+                        alt="Logo Money Minder"
+                        width={50}
+                        height={50}
+                        />
+                    <div className={styles.usernameLogin}>
+                        <label>Username:</label>
+                        <input type="text" value={username} onChange={handleUsernameChange} />
+                    </div>
+                    <div className={styles.passwordLogin}>
+                        <label>Password:</label>
+                        <input type="password" value={password} onChange={handlePasswordChange} />
+                    </div>
+                    <button className={styles.loginButton} onClick={handleLogin}>
+                        Login
+                    </button>
+                    </Card>
+                    
+                </div>
+            
+            </div> 
+            
         </div>
-      
     </main>
   );
-};
+}
